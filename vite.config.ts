5b95dbca86486887d416c4c91d15c9a1546d4b48
kmate <kmate@users.noreply.github.com>
2025-11-29 21:10:04 +0000

    deploy: 30f1f7004bb69ed66df09b6e1487b94b3f87537c

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-
-// Adjust base for GitHub Pages: '/<repo-name>/'
-// Using workspace folder name as default: 'puszacode-2025'
-export default defineConfig({
-  base: '/puszacode-2025/',
-});
